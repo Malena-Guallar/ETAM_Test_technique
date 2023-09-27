f316980ada71d86f2fd5549c2b57b7da5322410c
Malena Guallar <malena.guallar@gmail.com>
2023-09-27 17:01:49 +0200

    [style] Styled second slot mobile and desktop

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 new Glide(".glide", {
   type: "carousel",
   startAt: 0,
-  perView: 1,
+  perView: 1.1,
+  gap: 0,
   autoPlay: false,
 }).mount();
 
 const updateLayout = () => {
   const screenWidth = window.innerWidth;
 
-  const glideCarousel = document.querySelector(".glide");
-  const cardsRow = document.querySelector(".cards-row");
+  const glideCarousel = document.querySelector(".second-slot-mob");
+  const cardsRow = document.querySelector(".second-slot-desk");
 
   if (screenWidth <= 768) {
-
     cardsRow.style.display = "none";
   } else {
     glideCarousel.style.display = "none";
-    cardsRow.style.display = "block";
-  }
+      }
 };
 updateLayout(); 

374891a10f78ce388b63c76ee59a13f0f61df469
Malena Guallar <malena.guallar@gmail.com>
2023-09-27 15:15:54 +0200

    [feat] Implemented caroussel with Glide + function to detect browser size and display cards in caroussel or in rows

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-new Glide('.glide', {
-    type:'carousel',
-    startAt: 0,
-    perView: 1,
-    autoPlay: false,
+new Glide(".glide", {
+  type: "carousel",
+  startAt: 0,
+  perView: 1,
+  autoPlay: false,
+}).mount();
 
-}).mount()
+const updateLayout = () => {
+  const screenWidth = window.innerWidth;
+
+  const glideCarousel = document.querySelector(".glide");
+  const cardsRow = document.querySelector(".cards-row");
+
+  if (screenWidth <= 768) {
+
+    cardsRow.style.display = "none";
+  } else {
+    glideCarousel.style.display = "none";
+    cardsRow.style.display = "block";
+  }
+};
+updateLayout(); 

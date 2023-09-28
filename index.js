bdd6bf7f71e006fa777115c33d4f0b9b4ae345cd
Malena Guallar <malena.guallar@gmail.com>
2023-09-28 14:40:54 +0200

    [feat] Implemented toggle button function to hide and show details on fourth slot

--- index.js
+++ index.js
@@ -16,6 +16,24 @@ const updateLayout = () => {
     cardsRow.style.display = "none";
   } else {
     glideCarousel.style.display = "none";
-      }
+  }
 };
-updateLayout(); 
+updateLayout();
+
+let contentArray = document.querySelectorAll(".qa-content");
+
+let buttons = document
+  .querySelectorAll(".open-close-arrow")
+  .forEach((button, index) => {
+    button.addEventListener("click", function showDetails() {
+      let content = contentArray[index];
+
+      if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+        button.innerHTML = "︿";
+      } else {
+        content.style.display = "none";
+        button.innerHTML = "﹀";
+      }
+    });
+  });
